499e808f7ba4baeadec2ca73e3ceefeda9b5414e
Danny <unsafe521@gmail.com>
2022-12-28 10:47:52 +0800

    新发布

--- bot/bot.ts
+++ bot/bot.ts
@@ -5,7 +5,7 @@ const bot = new Bot(Deno.env.get("BOT_TOKEN")!); // <-- place your bot token ins
 
 // Listen for messages
 bot.command("start", (ctx) => ctx.reply("Welcome! Send me a photo!"));
-bot.on("message:text", (ctx) => ctx.reply("That is text and not a photo!"));
+bot.on("message:text", (ctx) => ctx.reply("That is text and not a photo! 新发布"));
 bot.on("message:photo", (ctx) => ctx.reply("Nice photo! Is that you?"));
 bot.on("edited_message", (ctx) =>
   ctx.reply("Ha! Gotcha! You just edited this!", {
