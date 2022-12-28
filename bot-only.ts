aece387e3969b38d52d9a8c27786512fdb98e578
Danny <unsafe521@gmail.com>
2022-12-28 11:04:33 +0800

    log local bot

--- bot-only.ts
+++ bot-only.ts
@@ -1,3 +1,6 @@
 import bot from "./bot/bot.ts";
+console.log("deleting web hook");
 await bot.api.deleteWebhook();
+console.log("delete web hook successfully");
+console.log("starting bot");
 bot.start();
