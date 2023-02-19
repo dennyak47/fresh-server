4a7f230bd2eda40f408cb78e1b5e2b03e94cfd42
Danny <unsafe521@gmail.com>
2023-02-19 18:18:15 +0800

    update

--- routes/api/joke.ts
+++ routes/api/joke.ts
@@ -1,5 +1,28 @@
 import { HandlerContext } from "$fresh/server.ts";
+// import * as postgres from "postgres/mod.ts";
+// // Get the connection string from the environment variable "DATABASE_URL"
+// const databaseUrl = Deno.env.get("DATABASE_URL")!;
+// console.log("databaseUrl", databaseUrl);
 
+// // Create a database pool with three connections that are lazily established
+// const pool = new postgres.Pool(databaseUrl, 3, true);
+
+// // Connect to the database
+// const connection = await pool.connect();
+// try {
+//   // Create the table
+//   await connection.queryObject`
+//     CREATE TABLE IF NOT EXISTS todos (
+//       id SERIAL PRIMARY KEY,
+//       title TEXT NOT NULL
+//     )
+//   `;
+//   console.log("create table success");
+// } finally {
+//   // Release the connection back into the pool
+//   connection.release();
+//   console.log("disconnect database");
+// }
 // Jokes courtesy of https://punsandoneliners.com/randomness/programmer-jokes/
 const JOKES = [
   "Why do Java developers often wear glasses? They can't C#.",
